1a1404358bae82f49ccf88b3b1d93a20dd3725cb
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-04-02 20:03:52 +0200

    customize admin panel

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,30 @@ from .models import Product, Brand
 
 # Register your models here.
 
-admin.site.register(Product)
-admin.site.register(Brand)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'sku',
+        'name',
+        'brand',
+        'rating',
+        'price',
+        'image',
+    )
+    search_fields =['name']
+    list_filter = ('sku',)
+    ordering = ('sku',)
+
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'other_name',
+    )
+    search_fields =['name']
+    list_filter = ('name',)
+    ordering = ('name',)
+
+#admin.site.register(Product)
+admin.site.register(Brand, BrandAdmin)
--- products/models.py
+++ products/models.py
@@ -35,5 +35,10 @@ class Product(models.Model):
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
+    class Meta:
+
+        ordering = ["sku"]
+        #verbose_name_plural = "AllBrands"
+
     def __str__(self):
         return self.sku
