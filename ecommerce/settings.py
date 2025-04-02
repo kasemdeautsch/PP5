00f44844a56eaab13459708e7831f373bc6a0fa7
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-04-02 13:05:12 +0200

    remove ram field and alter price and rating fields

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -15,8 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-print('BASE_DIR:', BASE_DIR)
-print('TEMPLATES_DIR:', TEMPLATES_DIR)
+#print('BASE_DIR:', BASE_DIR)
+#print('TEMPLATES_DIR:', TEMPLATES_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
