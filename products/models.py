289263b544d417e1692ab42d417b3cc288d6a592
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-04-02 13:05:12 +0200

    remove ram field and alter price and rating fields

--- products/models.py
+++ products/models.py
@@ -25,16 +25,15 @@ class Product(models.Model):
     processor = models.CharField(max_length=254)
     memory = models.CharField(max_length=254)
     storage = models.CharField(max_length=254)
-    ram = models.CharField(max_length=254)
     graphics = models.CharField(max_length=254)
     warranty = models.CharField(max_length=254, null=True, blank=True)
     operating_system = models.CharField(max_length=254, null=True, blank=True)
 
     availability = models.BooleanField(default=True, null=True, blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    rating = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.sku
