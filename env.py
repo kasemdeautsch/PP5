c617c6f08ca25af6fc5a283373e0a73f18b7b1f8
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-08 18:06:43 +0200

    Product model-Template: modify field-name of brand

--- env.py
+++ env.py
@@ -1,5 +1,3 @@
 import os
 
-# os.environ.setdefault("SECRET_KEY", "9tLVzm.YR4dEd$N%)}W1bpr>Mm^P}k")
 os.environ.setdefault("SECRET_KEY", "lp0|>i)]*H<F6q-xnAji:Rb?S(J)Po=hNEmVh1Bn9}FpFKS[k)jUcXE(2tPj/O*")
-# os.environ.setdefault('SECRET_KEY', 'UqhOevcjai')
