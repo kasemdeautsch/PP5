69889bb5277952b7a0b5fcb6467ffa38ebb2bde7
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-14 21:01:11 +0200

    checkout page - add overlay

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,6 +6,7 @@ from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
+
     """
     Update order total on lineitem update/create
     """
@@ -17,6 +18,7 @@ def update_on_save(sender, instance, created, **kwargs):
 
 @receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
+    
     """
     Update order total on lineitem delete
     """
