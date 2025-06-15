5cc1fb30d80b24474dd2f7a2bdd9a4b5226a4ad9
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-15 20:55:49 +0200

    finish webhook process

---
+++ env.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "lp0|>i)]*H<F6q-xnAji:Rb?S(J)Po=hNEmVh1Bn9}FpFKS[k)jUcXE(2tPj/O*")
+
+# os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51RZUB0PSaDnciGD0sUTsZISSLTIr3pNb1G2ZtjKtPXSpa9Eoza9Lspk2QtJrGcqOx3JGUuHR3326RJcOX63ScIBl00cpDtnMhw')
+# os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51RZUB0PSaDnciGD0zP7nMYiaK7IhzqaXwkyXfwPN2XqBpEvdlwKuImqygnQePGoKhFfjfDQhAHqF0oRVsIfGMkaF00I2FiojD4')
+
+os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51R4juVD3PlWdN0L5wO4WKiycMKOcUzNVVpFwU82D0CXeCZnDcJXqZbeBEzBpFDdqAWL9A1F5X6JntPclMeli4lpY00z00sXjJu')
+os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51R4juVD3PlWdN0L5SVX4afsAjRdTQIY2kkpCiKjb5lQv1wCyASSk4s15FqdviCoPwNOWV3SJOzT2hHfPKzMXh6DV00Th1ed6Xa')
+os.environ.setdefault('STRIPE_WH_SECRET', 'whsec_ea5636f83fb29ce449a2990ffa1fee27e79f567586891d52f3d25df164087a1a')
