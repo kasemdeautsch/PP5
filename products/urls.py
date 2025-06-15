e11c5f0cee00a9075b0461115c7b5ffd6835dccc
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-15 10:27:03 +0200

    remove env.py file

--- products/urls.py
+++ products/urls.py
@@ -7,3 +7,11 @@ urlpatterns = [
     path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail'),
 ]
+
+# import os
+
+#os.environ.setdefault("SECRET_KEY", "lp0|>i)]*H<F6q-xnAji:Rb?S(J)Po=hNEmVh1Bn9}FpFKS[k)jUcXE(2tPj/O*")
+
+#os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51RZUB0PSaDnciGD0sUTsZISSLTIr3pNb1G2ZtjKtPXSpa9Eoza#9Lspk2QtJrGcqOx3JGUuHR3326RJcOX63ScIBl00cpDtnMhw')
+#os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51RZUB0PSaDnciGD0zP7nMYiaK7IhzqaXwkyXfwPN2XqBpEvdlwKuImqygnQePGoKhFfjfDQhAHqF0oRVsIfGMkaF00I2FiojD4')
+
