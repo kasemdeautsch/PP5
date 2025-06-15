b468d4c91e9884c5fa0c7855f9dce33880f24cce
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-15 16:10:23 +0200

    create view to modify payment intent and post to it from js

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
@@ -7,12 +8,33 @@ from products.models import Product
 from .models import OrderLineItem, Order
 
 import stripe
+import json
 
 # Create your views here.
+
+@require_POST
+def cache_checkout_data(request):
+
+    print('cache_checkout_data received>>')
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
+
 
 
 def checkout(request):
 
+    print('checkout received>>')
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -89,6 +111,7 @@ def checkout_success(request, order_number):
     Handle successful checkout 
     """
 
+    print('checkout_success recieved>>')
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Order successful! \
