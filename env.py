3c893bb69c4c878d939a52f2205fb15706d41249
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-15 11:10:39 +0200

    add webhook handler

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("SECRET_KEY", "lp0|>i)]*H<F6q-xnAji:Rb?S(J)Po=hNEmVh1Bn9}FpFKS[k)jUcXE(2tPj/O*")
+
+os.environ.setdefault('STRIPE_PUBLIC_KEY', 'pk_test_51RZUB0PSaDnciGD0sUTsZISSLTIr3pNb1G2ZtjKtPXSpa9Eoza9Lspk2QtJrGcqOx3JGUuHR3326RJcOX63ScIBl00cpDtnMhw')
+os.environ.setdefault('STRIPE_SECRET_KEY', 'sk_test_51RZUB0PSaDnciGD0zP7nMYiaK7IhzqaXwkyXfwPN2XqBpEvdlwKuImqygnQePGoKhFfjfDQhAHqF0oRVsIfGMkaF00I2FiojD4')
