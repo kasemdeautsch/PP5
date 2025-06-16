6b6d9e30e129c0fbe2c312dc7a4eecea95368f9e
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-16 20:35:02 +0200

    Add profile app

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django_countries.fields import CountryField
 from decimal import Decimal
 from products.models import Product
+from profiles.models import userProfile
 
 # Create your models here.
 
@@ -12,6 +13,8 @@ from products.models import Product
 class Order(models.Model):
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(userProfile, on_delete=models.SET_NULL, 
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
