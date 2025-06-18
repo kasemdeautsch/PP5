cff0c8c83181dd481baded672074986c5dcc674f
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-18 22:52:46 +0200

    product admin - securing the views

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from checkout.models import Order
 from .models import UserProfile
 from .forms import UserProfileForm
 # Create your views here.
 
 
+@login_required
 def profile(request):
     """
     Displays the user's profile
