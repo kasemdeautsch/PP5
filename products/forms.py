4f721a1cc985c4b023ee4681840143a07594cb0e
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-22 18:55:37 +0200

    product admin - add css and javascript

--- products/forms.py
+++ products/forms.py
@@ -9,7 +9,7 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
-    image = forms.ImageField(label='Image-----', widget=CustomClearableFileInput, required=False)
+    image = forms.ImageField(label='Image--', widget=CustomClearableFileInput, required=False)
 
     # image = forms.ImageField(label='MyImage:------', required=False, widget=CustomClearableFileInput)
 
@@ -19,7 +19,9 @@ class ProductForm(forms.ModelForm):
 
         brands = Brand.objects.all()
         other_names = ((c.id, c.get_other_name()) for c in brands)
+        print('other_names: ', other_names)
+        print('brands: ', brands)
         self.fields['brand'].choices = other_names
-
+        print('self.fields.items(): ', self.fields.items())
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
--- products/widgets.py
+++ products/widgets.py
@@ -1,6 +1,7 @@
 from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
 
+
 class CustomClearableFileInput(ClearableFileInput):
 
     clear_checkbox_label = _("Remove")
