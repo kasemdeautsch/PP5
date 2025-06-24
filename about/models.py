28254d166d171245b6e15622ebfcb60dcbeeaa15
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-24 10:37:08 +0200

    create about, collaboraterequest models

---
+++ about/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+
+class About(models.Model):
+    """Stores a single about record"""
+
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    updated_on = models.DateTimeField(auto_now=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class CollaborateRequest(models.Model):
+    """Stores a single collaborate request"""
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField(max_length=5000)
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Collaboration request from: {self.name}'
