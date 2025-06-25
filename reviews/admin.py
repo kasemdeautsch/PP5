8fd64240c648dbd7db4f7b69259a6285f4336896
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-06-25 16:58:05 +0200

    Reviews - add tow more fields for date

--- reviews/admin.py
+++ reviews/admin.py
@@ -7,7 +7,7 @@ from .models import Reviews
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
     
-    list_display = ('name', 'rating', 'review')
+    list_display = ('name', 'rating', 'review', 'updated_on')
     
     class Meta:
         ordering = ['reviewq']
--- reviews/models.py
+++ reviews/models.py
@@ -7,7 +7,7 @@ class Reviews(models.Model):
 
     class Meta:
         verbose_name_plural = 'Reviews'
-        ordering = ['rating']
+        ordering = ['-updated_on']
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_reviews')
     name = models.CharField(max_length=254)
@@ -16,6 +16,7 @@ class Reviews(models.Model):
                                  null=True, blank=True)
     review = models.TextField(null=False, blank=False, default='',
                               max_length=5000)
-    
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f'{self.user.username} reviewed: {self.review}'
